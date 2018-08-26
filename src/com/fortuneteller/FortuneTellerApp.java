fd6845efb57e1b7432cdd0c5b79771fe87bd36c3
Andrea McBride <31637885+amcbride001@users.noreply.github.com>
2018-08-26 16:41:22 -0400

    added name request

--- src/com/fortuneteller/FortuneTellerApp.java
+++ src/com/fortuneteller/FortuneTellerApp.java
@@ -1,9 +1,16 @@
 package com.fortuneteller;
 
+import java.util.Scanner;
+
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// Create scanner to get user input	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your first name?");
+		String lastName = input.nextLine();
+		
 
 	}
 
